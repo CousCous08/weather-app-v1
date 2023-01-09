554d9ea94cb9003904588e3bcc2fafae15ef2b18
Marcus <mnl1@williams.edu>
2023-01-08 21:28:40 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '45eec33dddmshbcb6785701f1de2p1ea69djsn781cb53491d0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '815922fd3f51fda380368f5d562c2942'
